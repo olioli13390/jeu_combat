fdc745f7c484cc58b399fbceacbd1a6e7e8cf357
olioli13390 <olivier001@hotmail.fr>
2025-02-17 11:59:19 +0100

    first-commit

---
+++ main.js
@@ -0,0 +1,97 @@
+const prompt = require("prompt-sync")()
+
+const fighters = [{
+    nom: 'Sombre Lutin'
+}, {
+    nom: 'Guerrier du feu'
+}]
+
+const tabAttack = [{
+    attaque: 'Frappe rapide',
+    puissance: 10,
+
+},
+{
+    attaque: 'Soin léger',
+    puissance: -15,
+
+},
+{
+    attaque: 'Coup Puissant',
+    puissance: 20,
+
+},
+{
+    attaque: 'Frappe dévastatrice',
+    puissance: 30,
+
+}]
+
+function randomize(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let pvSombreLutin = 50
+let pvGuerrierDuFeu = 50
+while (pvGuerrierDuFeu > 0 && pvSombreLutin > 0) {
+
+    for (let i = 0; i < fighters.length; i++) {
+        if (i === 0) { // choix du joueur
+            console.log("Choisis ton attaque : ")
+            for (let j = 0; j < tabAttack.length; j++) {
+                console.log(`${j + 1} : ${tabAttack[j].attaque}`);
+            }
+
+            let choice = Number(prompt())
+
+            if (choice > 0 && choice <= tabAttack.length) {
+                let res = tabAttack[choice - 1]
+                switch (res.attaque) {
+                    case 'Frappe rapide':
+                        res.precision = randomize(1, 2);
+                        break;
+                    case 'Soin léger':
+                        res.precision = randomize(1, 3);
+                        break;
+                    case 'Coup Puissant':
+                        res.precision = randomize(1, 3);
+                        break;
+                    case 'Frappe dévastatrice':
+                        res.precision = randomize(1, 4);
+                        break;
+                }
+                
+                console.log(res.precision);
+
+                if (res.precision === 1) {
+                    console.log(`${fighters[i].nom} tente ${res.attaque} mais cela échoue`);
+                } else {
+                    pvGuerrierDuFeu -= res.puissance
+                    console.log(`${fighters[i].nom} lance ${res.attaque} et inflige ${res.puissance} de dégâts`);
+                }
+                console.log(`Il ne reste plus que ${pvGuerrierDuFeu} au Guerrier du feu `);
+
+            } else { /// erreur
+                console.log('Recommence en choisissant entre 1 et 4')
+                choice = Number(prompt());
+
+            }
+
+        } else { /// attaque automatique
+
+            let randomizeAttack = randomize(0, tabAttack.length - 1)
+            let resDeux = tabAttack[randomizeAttack]
+
+            if (resDeux.precision === 1) {
+                console.log(`${fighters[i].nom} lance ${resDeux.attaque} mais cela échoue !`);
+            } else {
+                pvSombreLutin -= resDeux.puissance
+                console.log(`${fighters[i].nom} inflige ${resDeux.puissance} avec ${resDeux.attaque}`);
+            }
+            console.log(`Il ne reste plus que ${pvSombreLutin} au Sombre Lutin `);
+        }
+    }
+
+}
+
+
